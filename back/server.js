5ba3aaedbfa7becb877c64724f2089c64e7a1ef8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-21 14:28:02 +0100

    dev: added log middleware

--- back/server.js
+++ back/server.js
@@ -4,6 +4,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use((req, res, next) => {
+  console.log('url:', req.path)
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
