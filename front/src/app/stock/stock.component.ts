4c6cbd2187a1385a2e38926fe777e95779273f43
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-22 17:34:10 +0100

    dev: added ngOnDestroy

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Article } from '../interfaces/article';
 
 @Component({
@@ -6,9 +6,13 @@ import { Article } from '../interfaces/article';
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss'],
 })
-export class StockComponent {
+export class StockComponent implements OnDestroy {
   articles: Article[] = [
     { id: 'a1', name: 'Pelle', price: 3.99, qty: 123 },
     { id: 'a2', name: 'Marteau', price: 5, qty: 34 },
   ];
+
+  ngOnDestroy(): void {
+    console.log('au revoir...');
+  }
 }
