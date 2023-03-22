732bf266659a966ca3fd339eb222be5fa508c268
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-22 17:27:11 +0100

    dev: added article loop

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent {
-
+  articles: Article[] = [
+    { id: 'a1', name: 'Pelle', price: 3.99, qty: 123 },
+    { id: 'a2', name: 'Marteau', price: 5, qty: 34 },
+  ];
 }
