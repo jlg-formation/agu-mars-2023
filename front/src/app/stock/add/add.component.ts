a0adf39aea63349ba1fdc39c2836add4abf18c2c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-23 14:34:31 +0100

    dev: added form management

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -8,4 +9,13 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 })
 export class AddComponent {
   faPlus = faPlus;
+
+  f = new FormGroup({
+    name: new FormControl('Truc', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    price: new FormControl(0, [Validators.required]),
+    qty: new FormControl(0, [Validators.required]),
+  });
 }
